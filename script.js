ff3aa1ea1c8d3e4a9a662c160c638b48f6c75166
Joyce <hellojoycerice@gmail.com>
2019-01-16 11:45:46 -0500

    minor tweaks prepring to start scripting

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 // javascript for hotel california
+
+// open session storage
+// keep track of which tiles have been visisted
+  // show appropriate tiles
+  // display appropriate background image
+// reset sessions storage when user clicks final "home" link
