da8bd2de999bb63a6a00b78fbe1ab30d4b4e97ac
Joyce <hellojoycerice@gmail.com>
2019-01-21 16:40:46 -0500

    adds the background tracker

--- script.js
+++ script.js
@@ -5,3 +5,15 @@
   // show appropriate tiles
   // display appropriate background image
 // reset sessions storage when user clicks final "home" link
+
+//state variables that must be stored
+let bgState = 1;
+
+//functions that apply appropriate styles
+function bgTracker() {
+  let theBod = document.getElementById("theBod");
+  if (bgState < 6) {
+    theBod.style.backgroundImage = "url(assets/hotel"+bgState+".jpg)";
+    bgState += 1;
+  } else {bgState = 1;}
+}
