bf11cd58e842ca9eb0b56007f70f6bbb02380984
Joyce <hellojoycerice@gmail.com>
2019-01-23 11:38:58 -0500

    adds neon sign blinking functionality :O

--- script.js
+++ script.js
@@ -17,3 +17,50 @@ function bgTracker() {
     bgState += 1;
   } else {bgState = 1;}
 }
+
+//TEMPLATED BLINKING
+function doABlink(blinker) {
+  let lit = 'assets/'+ blinker.id+'.png';
+  let unlit = 'assets/'+ blinker.id+'x.png';
+  darkIt(blinker, unlit);
+  setTimeout(function() {
+    lightIt(blinker, lit)
+  }, 200);
+  setTimeout(function() {
+    darkIt(blinker, unlit);
+  }, 600);
+  setTimeout(function() {
+    lightIt(blinker, lit)
+  }, 700);
+  setTimeout(function() {
+    darkIt(blinker, unlit);
+  }, 800);
+  setTimeout(function() {
+    lightIt(blinker, lit)
+  }, 900);
+}
+
+function lightIt(blinker, lit) {
+  blinker.src = lit;
+}
+
+function darkIt(blinker, unlit) {
+  blinker.src = unlit;
+}
+
+
+//WORKS, BUT NOT TEMPLATEABLE
+function blink(blinker) {
+  let oldSrc = 'assets/'+ blinker.id;
+  let blinked =  oldSrc+'x.png';
+  console.log('blinked fore');
+  blinker.src = blinked;
+  setTimeout(function() {
+    blinkBack(blinker, oldSrc);  // You used `el`, not `element`?
+  }, 500);
+}
+
+function blinkBack(blinker, oldSrc) {
+  blinker.src = oldSrc+'.png';
+  console.log('blinked back');
+}
